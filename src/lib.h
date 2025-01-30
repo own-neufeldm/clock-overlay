c07fd67f7269aa6f4db242f7478dd71a219c375d
Martin Neufeld <m.neufeld946@gmail.com>
2025-01-30 18:02:26 +0100

    refactor: document appstate

--- src/lib.h
+++ src/lib.h
@@ -6,45 +6,105 @@
 #include <time.h>
 
 /**
- * Application state for passing data throughout callbacks.
- *
- * \param windowFlags flags to use for this window, such as borderless.
- * \param defaultGeometry default geometry to use on startup and when resetting
- *                        the position. Use zero for width/height to fit window
- *                        to width/height of text. Use below zero for x/y to
- *                        place window at width/height of screen minus value.
- * \param defaultOpacity default opacity to use on startup and when resetting.
- * \param foregroundColor text color to use.
- * \param backgroundColor window color to use.
- * \param timeFormat time format to apply as defined by `strftime`, e.g. `%H%M`.
- * \param timeReference a reference value in given format for calculating text
- *                      size to fit window, e.g. `88:88` for format `%H:%M`.
- * \param fontFile relative path to True-Type-Font file for rendering text.
- * \param fontSize font size, text will be stretched to fit window if need be.
- *
+ * Application state for passing data throughout callbacks without globals.
  */
 typedef struct {
-  // style configured by developer
+  /**
+   * Flags to use for this window, such as borderless.
+   */
   SDL_WindowFlags windowFlags;
+
+  /**
+   * Default geometry to use on startup and when resetting the position. Use
+   * zero for width/height to fit window to width/height of text. Use below zero
+   * for x/y to place window at width/height of screen minus value.
+   */
   SDL_Rect defaultGeometry;
+
+  /**
+   * Default opacity to use on startup and when resetting.
+   */
   float defaultOpacity;
+
+  /**
+   * Text color to use.
+   */
   SDL_Color foregroundColor;
+
+  /**
+   * Window color to use.
+   */
   SDL_Color backgroundColor;
+
+  /**
+   * Time format to apply as defined by `strftime`, e.g. `%H%M`.
+   */
   const char *timeFormat;
+
+  /**
+   * A reference value in given format for calculating text size to fit window,
+   * e.g. `88:88` for format `%H:%M`.
+   */
   const char *timeReference;
+
+  /**
+   * Relative path to True-Type-Font file for rendering text.
+   */
   const char *fontFile;
+
+  /**
+   * Font size, text will be stretched to fit window if need be.
+   */
   int fontSize;
 
-  // resources managed by application
+  /**
+   * Main window, managed by the application.
+   */
   SDL_Window *window;
+
+  /**
+   * Default renderer, managed by the application.
+   */
   SDL_Renderer *renderer;
+
+  /**
+   * Default font, managed by the application.
+   */
   TTF_Font *font;
+
+  /**
+   * Current texture to render, containing `text`, managed by the application.
+   */
   SDL_Texture *texture;
+
+  /**
+   * Formatted time to draw, managed by the application.
+   */
   char text[BUFFER_LENGTH];
+
+  /**
+   * Is the user dragging the window? Managed by the application.
+   */
   bool changePosition;
+
+  /**
+   * Geometry to apply when drawing the next frame, managed by the application.
+   */
   SDL_Rect requestedGeometry;
+
+  /**
+   * Offset start position when window is dragged, managed by the application.
+   */
   SDL_Point relativeMousePosition;
+
+  /**
+   * Is the user changing the window's opacity? Managed by the application.
+   */
   bool changeOpacity;
+
+  /**
+   * Opacity to apply when drawing the next frame, managed by the application.
+   */
   float requestedOpacity;
 } AppState;
 
